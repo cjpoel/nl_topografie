b5e134280323400ee380d84125fa02826c7be3f9
cjpoel <39593983+cjpoel@users.noreply.github.com>
2026-02-27 13:50:51 +0100

    Add Noord-Nederland dataset with geographical data

---
+++ datasets/noord.js
@@ -0,0 +1,25 @@
+export const DATASET = {
+  id: "noord",
+  title: "Topografie – Noord-Nederland (kies provincies)",
+  mapImage: "noord_schoon2.png",
+  assetVersion: "20260227-1",
+  canvasWidth: 608,
+  canvasHeight: 425,
+  delayGoodMs: 800,
+  delayBadMs: 1200,
+  places: [
+    { name: "Delfzijl",   province: "Groningen", x: 464.7, y: 101.8 },
+    { name: "Groningen",  province: "Groningen", x: 402.3, y: 135.2 },
+    { name: "Veendam",    province: "Groningen", x: 457.5, y: 168.5 },
+
+    { name: "Leeuwarden", province: "Friesland", x: 262.7, y: 141.1 },
+    { name: "Drachten",   province: "Friesland", x: 319.3, y: 170.7 },
+    { name: "Sneek",      province: "Friesland", x: 240.3, y: 190.4 },
+    { name: "Heerenveen", province: "Friesland", x: 286.5, y: 212.4 },
+
+    { name: "Assen",      province: "Drenthe",   x: 401.2, y: 201.0 },
+    { name: "Emmen",      province: "Drenthe",   x: 462.8, y: 265.2 },
+    { name: "Hoogeveen",  province: "Drenthe",   x: 385.7, y: 281.7 },
+    { name: "Meppel",     province: "Drenthe",   x: 335.7, y: 290.2 },
+  ]
+};
